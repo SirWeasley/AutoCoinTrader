5b2094007e780cd42382e33420405e8fa22c7b1f
Alvin Pearson <Weasleyafslaan@gmail.com>
2016-08-08 22:19:13 +0200

    Rejected commit: Default

---
+++ src/main/java/za/co/bots/BotInterface.groovy
@@ -0,0 +1,10 @@
+package za.co.bots
+
+/**
+ * Created by Alvin on 08 Aug 2016.
+ */
+interface BotInterface {
+
+    def process()
+
+}
---
+++ src/main/java/za/co/bots/OrderBookAnalysisBot.groovy
@@ -0,0 +1,11 @@
+package za.co.bots
+
+/**
+ * Created by Alvin on 08 Aug 2016.
+ */
+class OrderBookAnalysisBot {
+
+    def process(){
+
+    }
+}
